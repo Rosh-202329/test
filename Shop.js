4460c963f1799d22d0ebb72f15289666a70bc3f1
Rosh-202329 <155441430+Rosh-202329@users.noreply.github.com>
2024-02-12 23:04:06 -0800

    Add files via upload

---
+++ Shop.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Shop(){
+    return(
+        <div>
+           <p>Restaurent landing pge landing Home Gallery</p>
+          
+           <div >
+             <h2>Only Quality food</h2>
+             <h5>bjsbjsskvnkvkvkkkjkjjjk
+                bssnnvnvkkvnkvnkvnkvnknv
+             </h5>
+           </div>
+           <div>
+            <h5>our Story</h5>
+            <h2>Welcome to Royal</h2>
+            <h5 className="new">afgghhjjhkkk
+                afhghhkjjl
+                sgdgfhgfjhk
+                affdggfgjhkjk
+            </h5>
+           </div>
+        </div>
+
+    );
+}
+export default Shop;
