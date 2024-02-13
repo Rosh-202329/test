c77b422b7ca023b095a7f2b3042c8885eeb1b532
Rosh-202329 <155441430+Rosh-202329@users.noreply.github.com>
2024-02-12 22:26:20 -0800

    Add files via upload

---
+++ menuApi.js
@@ -0,0 +1,75 @@
+const Menu = [
+    {
+        id:1,
+        image: "images/maggi.jpg",
+        name: "maggi",
+        category: "breakfast",
+        price: "12rs",
+        description: "I love maggi really lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh."
+    },
+    {
+        id:2,
+        image: "images/allupakoda.jpg",
+        name: "allu pkoda",
+        category: "evening",
+        price: "20rs",
+        description: "I love allu pkoda lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh "
+    },
+    {
+        id:3,
+        image: "images/corn.webp",
+        name: "corn",
+        category: "breakfast",
+        price: "10rs",
+        description: "I love corn lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    },
+    {
+        id:4,
+        image: "images/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "50rs",
+        description: "I love chola puri lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    },
+    {
+        id:5,
+        image: "images/pizza.jpg",
+        name: "pizza",
+        category: "evening",
+        price: "100rs",
+        description: "I love pizza lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    },
+    {
+        id:6,
+        image: "images/nonveg.jpg",
+        name: "nonveg",
+        category: "dinner",
+        price: "200rs",
+        description: "I love nonveg lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    },
+    {
+        id:7,
+        image: "images/sweet.jpg",
+        name: "sweets",
+        category: "dinner",
+        price: "60rs",
+        description: "I love sweets lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    },
+    {
+        id:8,
+        image: "images/rajmarice.jpg",
+        name: "rajma rice",
+        category: "lunch",
+        price: "100rs",
+        description: "I love rajma rice lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    },
+    {
+        id:9,
+        image: "images/samosa.webp",
+        name: "samosa",
+        category: "breakfast",
+        price: "12rs",
+        description: "I love samosa lorem ipsum dolor sit amet confhbvf adivjjkkk totam fhfgh cfgfh"
+    }
+];
+export default Menu;
